f4fea1442b70b5e61b3b32aea13bd0e93a2c46f7
Rafael Vareto <rafaelvareto@gmail.com>
2017-05-21 14:41:48 -0300

    Python: Adding verification pairs to LFW and PubFig

--- python/auxiliar.py
+++ python/auxiliar.py
@@ -24,6 +24,45 @@ def load_txt_file(file_name):
         this_list.append(components)
     return this_list
 
+def read_fold_file(file_name):
+with open(file_name) as file_input:
+    neg_folds = dict()
+    pos_folds = dict()
+    file_list = file_input.readlines()
+    for index in range(len(file_list)):
+        file_list[index] = file_list[index].replace(' ', '_').strip().split('\t')
+    
+    index = 0;
+    while str(file_list[index][0]).startswith('#'):
+        index += 1
+    cur_fold = 0
+    num_folds = int(file_list[index][0])
+    index += 1
+    
+    while index < len(file_list):
+        if not str(file_list[index][0]).startswith('#'):
+            num_pos = int(file_list[index][0])
+            num_neg = int(file_list[index][1])
+            index += 1
+            
+            pos_list = list()
+            for inner in range(index, num_pos + index):
+                pos_list.append(file_list[inner])
+                index += 1
+            
+            neg_list = list()
+            for inner in range(index, num_neg + index):
+                neg_list.append(file_list[inner])
+                index += 1
+                
+            pos_folds[cur_fold] = pos_list
+            neg_folds[cur_fold] = neg_list
+            cur_fold += 1
+        else:
+            index += 1
+
+    return pos_folds, neg_folds
+
 
 def split_known_unknown_sets(complete_tuple_list, known_set_size=0.5):
     label_set = set()
