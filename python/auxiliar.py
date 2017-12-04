084166ab333ef7a8d357121f4c58183058f09307
Rafael Vareto <rafaelvareto@gmail.com>
2017-12-04 17:19:56 -0200

    Adding SVM cross-dataset version

--- python/auxiliar.py
+++ python/auxiliar.py
@@ -296,6 +296,29 @@ def learn_plsh_cv_model(features, dictionary, pos_split, neg_split):
     return (model, (pos_split, neg_split))
 
 
+def learn_svmh_cv_model(features, dictionary, pos_split, neg_split):
+    matrix_x = []
+    matrix_y = []
+    
+    for pos in pos_split:
+        index_a = dictionary[pos[0]]
+        index_b = dictionary[pos[1]]
+        diff_feat = np.absolute(np.subtract(features[index_a], features[index_b]))
+        matrix_x.append(diff_feat)
+        matrix_y.append(1)
+        
+    for neg in neg_split:
+        index_a = dictionary[neg[0]]
+        index_b = dictionary[neg[1]]
+        diff_feat = np.absolute(np.subtract(features[index_a], features[index_b]))
+        matrix_x.append(diff_feat)
+        matrix_y.append(0)
+
+    classifier = SVR(C=1.0,kernel='linear')
+    model = classifier.fit(np.array(matrix_x), np.array(matrix_y))
+    return (model, (pos_split, neg_split))
+
+
 def generate_probe_histogram(individuals, values, extra_name):
     plt.clf()
     plt.bar(range(len(individuals)), values)
--- python/bash/run_verification_cross_HPLS.sh
+++ python/bash/run_verification_cross_HPLS.sh
@@ -1,34 +1,92 @@
 #!/bin/bash
 
 #DF
+echo 'PUBFIG-DEV, LFW: DF, Hash_samples = 50, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 005 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 010 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 020 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 030 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 050 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 070 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 100 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 300 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 500 -hs 50 -it 10
+
 echo 'PUBFIG-DEV, LFW: DF, Hash_samples = 100, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 005 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 010 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 020 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 030 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 050 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 070 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 100 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 300 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 500 -hs 100 -it 10
 
 echo 'PUBFIG-DEV, LFW: DF, Hash_samples = 300, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 005 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 010 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 020 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 030 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 050 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 070 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 100 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 300 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 500 -hs 300 -it 10
 
 echo 'PUBFIG-DEV, LFW: DF, Hash_samples = 500, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 005 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 010 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 020 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 030 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 050 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 070 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 100 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 300 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/lfw/lfw_pairs.txt -d lfw -ftr PUBFIG-DEV-DEEP.bin -fts LFW-DEEP.bin -hm 500 -hs 500 -it 10
 
 
 #DF
+echo 'PUBFIG-DEV, PUBFIG-EVAL: DF, Hash_samples = 50, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 005 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 010 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 020 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 030 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 050 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 070 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 100 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 300 -hs 50 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 500 -hs 50 -it 10
+
 echo 'PUBFIG-DEV, PUBFIG-EVAL: DF, Hash_samples = 100, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 005 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 010 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 020 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 030 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 050 -hs 100 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 070 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 100 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 300 -hs 100 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 500 -hs 100 -it 10
 
 echo 'PUBFIG-DEV, PUBFIG-EVAL: DF, Hash_samples = 300, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 005 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 010 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 020 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 030 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 050 -hs 300 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 070 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 100 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 300 -hs 300 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 500 -hs 300 -it 10
 
 echo 'PUBFIG-DEV, PUBFIG-EVAL: DF, Hash_samples = 500, Iterations = 10'
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 005 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 010 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 020 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 030 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 050 -hs 500 -it 10
+python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 070 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 100 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 300 -hs 500 -it 10
 python ../HPLS_verification_cross_load.py -p ../features/ -c ../datasets/pubfig/pubfig_full.txt -d pubfig -ftr PUBFIG-DEV-DEEP.bin -fts PUBFIG-EVAL-DEEP.bin -hm 500 -hs 500 -it 10
